852e156546faf95b6f6d63ed032fda2d705a1751
Vivekanand Soundalge <vivekanand.soundalge@mavenir.com>
2020-09-05 18:36:52 +0530

    FizzBuzz

--- src/com/codewithvivek/Main.java
+++ src/com/codewithvivek/Main.java
@@ -1,7 +1,20 @@
 package com.codewithvivek;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        System.out.print("Number: ");
+        Scanner scanner = new Scanner(System.in);
+        byte number = scanner.nextByte();
+        if (true == (number % 5 == 0 && number % 3 == 0)) {
+            System.out.println("FizzBuzz");
+        } else if (number % 5 == 0) {
+            System.out.println("Fizz");
+        } else if (number % 3 == 0) {
+            System.out.println("Buzz");
+        } else {
+            System.out.println(number);
+        }
     }
 }
